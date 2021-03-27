c983cbe84874f0718146333791d27871777288ae
Amir Tal <46451642+amirtal75@users.noreply.github.com>
2021-03-27 15:41:05 +0000

    Add files via upload

---
+++ CiscoSNMPGet.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Net;
+using SnmpSharpNet;
+using NLog;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+// Based on snmpshart for .Net
+// http://www.snmpsharpnet.com
+namespace CiscoSnmpGet
+{
+    class Program
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            InitLogger(true);
+            AuthenticationDigests authDigest = AuthenticationDigests.None;
+            PrivacyProtocols privacyprotocol = PrivacyProtocols.None;
+            string ip = "172.19.27.8";
+            int port = 161;
+            VbCollection vbCollection = null;
+            string community = "public";
+            UdpTarget target = null;
+            int version = 2;
+            SnmpV2Packet result2 = null;
+            int ErrorStatus = -1;
+            int ErrorIndex = -1;
+
+            string sidbase2 = "1.3.6.1.4.1.1429.2.2.6.5.12.3.1.2.";
+            string NameBase = "1.3.6.1.4.1.1429.2.2.6.5.12.3.1.4.";
+            string sidbase = "1.3.6.1.4.1.8813.2.366";
+            try
+            {
+                String snmpAgent = "172.19.27.8";
+                String snmpCommunity = "public";
+                SimpleSnmp snmp = new SimpleSnmp(snmpAgent, snmpCommunity);
+                // Create a request Pdu
+                Pdu pdu = new Pdu();
+                pdu.Type = PduType.GetNext;
+                List<string> lst1 = new List<string>();
+                List<string> lst2 = new List<string>();
+
+                string sid = "";
+                string channelName = "";
+
+                Dictionary<string, string> channelList = new Dictionary<string, string>();
+                Dictionary<Oid, AsnType> result = null;
+
+                for (int i = 0; i < 1; i++)
+                {
+                    sid = "";
+                    channelName = "";
+                    result = snmp.GetNext(SnmpVersion.Ver2,new string[]{ (sidbase + i) });
+                    if (result == null)
+                    {
+                        logger.Debug("Request failed.");
+                    }
+                    else
+                    {
+                        foreach (KeyValuePair<Oid, AsnType> entry in result)
+                        {
+                            if (entry.Key.ToString().Contains(sidbase))
+                            {
+                                sid = entry.Value.ToString();
+                            }
+                            else i = 100;
+                        }
+                    }
+                    result = snmp.GetNext(SnmpVersion.Ver2, new string[] { (NameBase + i) });
+                    if (result == null)
+                    {
+                        logger.Debug("Request failed.");
+                    }
+                    else
+                    {
+                        foreach (KeyValuePair<Oid, AsnType> entry in result)
+                        {
+                            if (entry.Key.ToString().Contains(NameBase))
+                            {
+                                channelName = entry.Value.ToString();
+                            }
+                        }
+                    }
+
+                    if (!sid.Equals("") && !channelName.Equals(""))
+                    {
+                        channelList.Add(sid, channelName);
+                    }
+                }
+
+                foreach (var item in channelList)
+                {
+                    logger.Debug(item.Key + ":" + item.Value);
+                }
+            }
+            catch (Exception e)
+            {
+
+                logger.Debug(e.Message);
+                logger.Debug(e.StackTrace);
+
+            }
+            /*
+            try
+            {
+                VbCollection coll = new VbCollection();
+                coll.Add("1.3.6.1.4.1.1429.2.2.6.5.2.1.1.1");
+                coll.Add("1.3.6.1.4.1.1429.2.2.6.5.2.1.1.2");
+                Pdu pdu = new Pdu(coll, PduType.GetBulk, 0);
+                pdu.MaxRepetitions = 10;
+                String snmpAgent = "172.19.54.30";
+                String snmpCommunity = "public";
+                SimpleSnmp snmp = new SimpleSnmp(snmpAgent, snmpCommunity);
+                for (int i = 0; i < 1; i++)
+                {
+                    Dictionary<Oid, AsnType> result = snmp.GetBulk(pdu);
+                    
+                    if (result == null)
+                    {
+                        logger.Debug("Request failed.");
+                    }
+                    else
+                    {
+                        logger.Debug("result size: " + result.Count);
+                        string filename = "D:\\Amir\\SNMP " + i + ".txt";
+                        using (System.IO.StreamWriter file =
+                        new System.IO.StreamWriter(filename))
+                        {
+                            foreach (KeyValuePair<Oid, AsnType> entry in result)
+                            {
+                                var x = entry.Value;
+                                file.WriteLine("{0} = {1}: {2}", entry.Key.ToString(), SnmpConstants.GetTypeName(entry.Value.Type),
+                                entry.Value.ToString());
+                            }
+                        }
+
+                    }
+                }
+                
+               
+            }
+            catch (Exception e)
+            {
+
+                logger.Debug(e.Message);
+                logger.Debug(e.StackTrace);
+            }
+            */
+            /*try
+            {
+                IpAddress ipa = new IpAddress(ip);
+                target = new UdpTarget((IPAddress)ipa);
+                target.Port = port;
+                // Construct a Protocol Data Unit (PDU)
+                Pdu pdu = new Pdu(PduType.Get);
+                // Set the request type (default is Get)
+                // pdu.Type = PduType.Get;
+                // Add variables you wish to query
+                pdu.VbList.Add(oid); //oid);
+                OctetString communityOctet = new OctetString(community);
+                AgentParameters param = new AgentParameters(communityOctet);
+
+                
+                
+                param.Version = SnmpVersion.Ver2;
+                logger.Debug("before get");
+                result2 = (SnmpV2Packet)target.Request(pdu, param);
+                logger.Debug("after get");
+
+                vbCollection = result2.Pdu.VbList;
+                ErrorStatus = result2.Pdu.ErrorStatus;
+                ErrorIndex = result2.Pdu.ErrorIndex;
+               
+                if (ErrorStatus != 0)
+                {
+                    // agent reported an error with the request
+                    logger.Debug("Error in SNMP reply. Error {0} index {1}",
+                        ErrorStatus, ErrorIndex);
+                }
+                else
+                {
+                    if (vbCollection != null)
+                    {
+                        foreach (Vb v in vbCollection)
+                        {
+                            logger.Debug("OID={0}\r\nType={1}\r\nValue:{2}",
+                                v.Oid.ToString(),
+                                SnmpConstants.GetTypeName(v.Value.Type), v.Value.ToString());
+                        }
+                    }
+                    else logger.Debug("VB COLLECTION IS NULL");
+                }
+                target.Close();
+            }
+            catch (Exception ex)
+            {
+                if (target != null)
+                    target.Close();
+                logger.Debug("Exception: " + ex.Message + " type: " + ex.GetType().ToString());
+            }
+                */
+            /*
+            string oid = "";
+            string ip = "";
+            int port = 161;
+            string securityuser = "";
+            string authenticationdigest = "";
+            AuthenticationDigests authDigest = AuthenticationDigests.None;
+            string authenticationpassword = "";
+            PrivacyProtocols privacyprotocol = PrivacyProtocols.None;
+            string privacyprotocolstring = "";
+            string privacyprotocolpassword = "";
+            string community = "";
+            int version = -1;
+            SnmpV3Packet result3;
+            SnmpV1Packet result1;
+            SnmpV2Packet result2;
+            
+            int ErrorStatus = -1;
+            int ErrorIndex = -1;
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Specify arguments:\r\n" +
+                "-v:version (Example: -v:1,-v:2c, -v:3)\r\n" +
+                "-r:IP (Example: -r:192.168.1.0)\r\n" +
+                "-p:port (Optional, default 161, example: -p:555\r\n" +
+                "-o:OID (Example: -o:1.3.6.1.2.1.1.2.0" +
+                "-c:community_string (Example: -c:public)\r\n" +
+                "-ap:AuthenticationDigest SHA/MD5(Example: -ap:SHA)\r\n" +
+                "-aw:AuthenticationPassword\r\n" +
+                "-pp:PrivacyProtocol DES/AES128/AES192/AES256/TripleDES (Example: -pp:AES128)\r\n" +
+                "-pp:PrivacyPassword\r\n" +
+                "-sn:SecurityName");
+                return;
+            }
+            else
+            {
+                foreach (string arg in args)
+                {
+                    if (arg.IndexOf("-v:") == 0)
+                    {
+                        if (arg.Substring(3) == "1")
+                            version = 1;
+                        if (arg.Substring(3) == "2c")
+                            version = 2;
+                        if (arg.Substring(3) == "3")
+                            version = 3;
+                    }
+                    if (arg.IndexOf("-o:") == 0)
+                    {
+                        oid = arg.Substring(3);
+                    }
+                    if (arg.IndexOf("-r:") == 0)
+                    {
+                        ip = arg.Substring(3);
+                    }
+                    if (arg.IndexOf("-sn:") == 0)
+                    {
+                        securityuser = arg.Substring(4);
+                    }
+                    if (arg.IndexOf("-c:") == 0)
+                    {
+                        community = arg.Substring(3);
+                    }
+                    if (arg.IndexOf("-p:") == 0)
+                    {
+                        string ports = arg.Substring(3);
+                        try
+                        {
+                            port = Int32.Parse(ports);
+                        }
+                        catch (Exception ex)
+                        {
+                            port = -1;
+                            Console.WriteLine("Not correct port value: " + ports);
+                            break;
+                        }
+                    }
+                    if (arg.IndexOf("-ap:") == 0)
+                    {
+                        authenticationdigest = arg.Substring(4);
+                        if (String.Compare(authenticationdigest, "SHA", true) == 0)
+                            authDigest = AuthenticationDigests.SHA1;
+                        if (String.Compare(authenticationdigest, "MD5", true) == 0)
+                            authDigest = AuthenticationDigests.MD5;
+                    }
+                    if (arg.IndexOf("-aw:") == 0)
+                    {
+                        authenticationpassword = arg.Substring(4);
+                    }
+                    if (arg.IndexOf("-pp:") == 0)
+                    {
+                        privacyprotocolstring = arg.Substring(4);
+                        if (String.Compare(privacyprotocolstring, "DES", true) == 0)
+                            privacyprotocol = PrivacyProtocols.DES;
+                        if (String.Compare(privacyprotocolstring, "AES128", true) == 0)
+                            privacyprotocol = PrivacyProtocols.AES128;
+                        if (String.Compare(privacyprotocolstring, "AES192", true) == 0)
+                            privacyprotocol = PrivacyProtocols.AES192;
+                        if (String.Compare(privacyprotocolstring, "AES256", true) == 0)
+                            privacyprotocol = PrivacyProtocols.AES256;
+                        if (String.Compare(privacyprotocolstring, "TripleDES", true) == 0)
+                            privacyprotocol = PrivacyProtocols.TripleDES;
+                    }
+                    if (arg.IndexOf("-pw:") == 0)
+                    {
+                        privacyprotocolpassword = arg.Substring(4);
+                    }
+                }
+            }
+            if (port == -1)
+            {
+                Console.WriteLine("Not valid port: " + port.ToString());
+                return;
+            }
+            if ((version < 1) || (version > 3))
+            {
+                Console.WriteLine("Not valid version: ");
+                return;
+            }
+            */
+
+
+        }
+        public static NLog.Logger InitLogger(bool debug)
+        {
+            var config = new NLog.Config.LoggingConfiguration();
+            string folder = "D:\\Amir\\";
+            var x = DateTime.Now.ToString().Replace('_', ':');
+            var y = x.Replace('/', '.');
+            string filename = "SNMP - " + y + ".txt";
+
+            // Targets where to log to: File and Console
+            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = folder + filename };
+
+            // Rules for mapping loggers to targets            
+            if (debug)
+            {
+                config.AddRule(NLog.LogLevel.Debug, NLog.LogLevel.Fatal, logfile);
+            }
+            else config.AddRule(NLog.LogLevel.Info, NLog.LogLevel.Fatal, logfile);
+
+            //NLog.LogManager.Setup().SetupInternalLogger(s =>
+            //s.SetMinimumLogLevel(LogLevel.Trace).LogToFile(@"D:\Amir\Hello.txt"));
+            // Apply config           
+            NLog.LogManager.Configuration = config;
+            return NLog.LogManager.GetCurrentClassLogger();
+
+        }
+    }
+}
+
+
---
+++ QuintechSeleniumAutomate.cs
@@ -0,0 +1,292 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Opera;
+using OpenQA.Selenium;
+using NLog;
+using OpenQA.Selenium.Support.UI;
+using ExpectedConditions = SeleniumExtras.WaitHelpers.ExpectedConditions;
+using System.Diagnostics;
+
+namespace QuintechSeleniumAutomate
+{
+    class Program
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        static async void Main(string[] args)
+        {
+            string testArgs = "*NOC* RF Room Out 27 (^*^) NOC IRD 4;NOC Physical Spec;NOC SED Spec";
+            Dictionary<int, int> x = new Dictionary<int, int>();
+            int[] asd = new int[128];
+            x.Keys.CopyTo(asd, 0);
+            for (int i = 0; i < asd.Length; i++)
+            {
+                var t = asd[i];
+            }
+            InitLogger(true);
+            var startTime = DateTime.Now;
+            logger.Debug("Starting log for the Quinteck lock script at: " + DateTime.Now);
+            logger.Debug("Number of arguments received: " + args.Length);
+
+            string[] arguments;
+            string[] seperator = { " (^*^) " };
+            if (args.Length > 0)
+            {
+                logger.Debug("arggument received: " + args.Length);
+                logger.Debug("args[0] = " + args[0]);
+                arguments = args[0].Split(seperator, StringSplitOptions.None);
+            }
+            else
+            {
+                logger.Debug("No argument received, using default: " + testArgs);
+                arguments = testArgs.Split(seperator, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            string inputName = arguments[0];
+            logger.Debug("Chosen matrix input: " + inputName);
+
+            string[] outputarray = arguments[1].Split(';');
+            List<string> outputNames = new List<string>(outputarray);
+            foreach (var item in outputNames)
+            {
+                logger.Debug("Chosen matrix output: " + item);
+
+            }
+            try
+            {
+                using (var driver = new ChromeDriver())
+                {
+                    driver.Navigate().GoToUrl("https://172.19.19.30/?GetScreen=login");
+                    SecurityBypass(driver);
+                    Login(driver);
+                    driver.Navigate().GoToUrl("https://172.19.19.30/?GetScreen=crosspoints");
+                    
+                    int tries = 10;
+                    Thread.Sleep(1000);
+                    while (BugCheck(driver))
+                    {
+                        Login(driver);
+                        driver.Navigate().GoToUrl("https://172.19.19.30/?GetScreen=crosspoints");
+                        if (tries == 0)
+                        {
+                            throw new Exception("Maximum bug fix retries was reached");
+                        }
+                        tries--;
+                    }
+                    logger.Debug("input url found " + driver.Url);
+                    PressOnInput(driver,inputName);
+                    PressOnOutputs(driver, outputNames);
+                    SaveCrossPoints(driver);
+
+                    logger.Debug("Run duration: " + (DateTime.Now - startTime));
+                }
+            }
+            catch (WebDriverException e)
+            {
+                logger.Debug(e.Message);
+                logger.Debug(e.StackTrace);
+                throw e;
+            }
+            catch (Exception e)
+            {
+                logger.Debug(e.Message);
+                logger.Debug(e.StackTrace);
+                throw e;
+            }
+        }
+        public static void SaveCrossPoints(IWebDriver driver)
+        {
+            string elementName = "SaveCrossPoints Function:";
+            logger.Debug(elementName);
+
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+            logger.Debug("Saving crosspoints");
+            wait.Until(ExpectedConditions.ElementExists(By.LinkText("Save"))).Click();
+        }
+        public static void PressOnOutputs(IWebDriver driver, List<string> outputNames)
+        {
+            string elementName = "PressOnOutput";
+            string outputName = "";
+            string outputXPath = "";
+            string parentXPath = "";
+            string xPath_outputNumber = "";
+            string outputNumber = "";
+            bool found = false;
+            int attemptNumber = 0;
+
+            for (int i = 0; i < outputNames.Count; i++)
+            {
+                outputName = outputNames[i];
+                outputXPath = "//td[contains(.,'" + outputName + "')]";
+                parentXPath = outputXPath + "/parent::node()";
+                xPath_outputNumber = parentXPath + "/*[2]";
+
+                found = false;
+                attemptNumber = 0;
+                logger.Debug("Bug check from output press");
+                BugCheck(driver);
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+                var outputElement = wait.Until(ExpectedConditions.ElementExists(By.XPath(outputXPath)));
+
+                var parentElement = driver.FindElement(By.XPath(parentXPath));
+
+                string className = parentElement.GetAttribute("class");
+                logger.Debug(outputName + " className: " + className);
+                
+                bool isChecked = className.Equals("checkable checked");
+                logger.Debug("Element {0} select status: " + isChecked, outputName);
+                if (!isChecked)
+                {
+                    outputElement.Click();
+                    Thread.Sleep(100);
+                    isChecked = parentElement.GetAttribute("class").Equals("checkable checked");
+                    logger.Debug(outputName + " after click state is: " + isChecked);
+                }
+            }
+        }
+        public static int PressOnInput(IWebDriver driver, string inputName)
+        {
+            string elementName = "PressOnInput Function";
+            logger.Debug(elementName);
+
+            string inputXPath = "//td[contains(.,'" + inputName + "')]";
+            string parentXPath = inputXPath + "/parent::node()";
+            string xPath_inputNumber = parentXPath + "/*[1]";
+            string inputNumber = "";
+
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            wait.Until(ExpectedConditions.ElementExists(By.XPath(inputXPath))).Click();
+            logger.Debug("After click");
+            inputNumber = driver.FindElement(By.XPath(xPath_inputNumber)).GetAttribute("innerHTML");
+
+            logger.Debug("Wait for output page to load by waiting on the last output in the output table");
+            try
+            {
+                logger.Debug("Trying wait with 4 seconds");
+                wait = new WebDriverWait(driver, TimeSpan.FromSeconds(4));
+                wait.Until(ExpectedConditions.ElementExists(By.Id("Output127")));
+            }
+            catch (Exception e)
+            {
+                logger.Debug("Bug check from input press");
+                BugCheck(driver);
+            }
+            logger.Debug("returning from input press");
+            return Int32.Parse(inputNumber);
+        }
+        public static bool BugCheck(IWebDriver driver)
+        {
+            string elementName = "BugCheck Function";
+            logger.Debug(elementName);
+            Thread.Sleep(3000);
+            var x = "";
+            try
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+                logger.Debug("Searching login element");
+                x = wait.Until(ExpectedConditions.ElementExists(By.XPath("//input[@name='login']"))).TagName;
+                logger.Debug("login element found - bug occrured");
+                return true;
+            }
+            catch (WebDriverException e)
+            {
+                logger.Debug("Bug not occured ,{0} found ",x);
+                logger.Debug("Bug not occured ,{0} found ", driver.Url);
+                return false;
+            }
+
+        }
+        public static void Login(IWebDriver driver)
+        {
+            string elementName = "Login Function";
+            logger.Debug(elementName);
+            SendUser(driver);
+            SendPassword(driver);
+
+            logger.Debug("Clicking login");
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));            
+            wait.Until(ExpectedConditions.ElementExists(By.XPath("//input[@name='login']"))).Click();
+
+            try
+            {
+                logger.Debug("Waiting for page to load by waiting on crosspoints element dor 3 seconds");
+                wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+                wait.Until(ExpectedConditions.ElementExists(By.LinkText("Crosspoints")));
+            }
+            catch (WebDriverException e)
+            {
+                logger.Debug("Page did not load, waiting for bug check");
+            }
+
+        }
+        public static void SendPassword(IWebDriver driver)
+        {
+            string elementName = "SendPassword Function:";
+            logger.Debug(elementName);
+
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+            logger.Debug("Sending password keys");
+            wait.Until(ExpectedConditions.ElementExists(By.Id("password"))).SendKeys("mcr");
+        }
+        public static void SendUser(IWebDriver driver)
+        {
+            string elementName = "SendUser Function:";
+            logger.Debug(elementName);
+
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+            logger.Debug("Sending user keys");
+            wait.Until(ExpectedConditions.ElementExists(By.Id("username"))).SendKeys("mcr");
+        }
+        public static void SecurityBypass(IWebDriver driver)
+        {
+            string elementName = "SecurityBypass Function";
+            logger.Debug(elementName);
+            try
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+                logger.Debug("Clicking advanced button");
+                wait.Until(ExpectedConditions.ElementExists(By.Id("details-button"))).Click();
+                logger.Debug("Clicking proceed button");
+                wait.Until(ExpectedConditions.ElementExists(By.Id("proceed-link"))).Click();
+                logger.Debug("Ovverided Security Page");
+            }
+            catch (WebDriverException)
+            {
+                logger.Debug("No security screen encountered");
+            }
+        }
+        public static NLog.Logger InitLogger(bool debug)
+        {
+            var config = new NLog.Config.LoggingConfiguration();
+            string folder = "D:\\Amir\\";
+            var x = DateTime.Now.ToString().Replace('_', ':');
+            var y = x.Replace('/', '.');
+            string filename = "Quintech Selenium Log - " + y + ".txt";
+
+            // Targets where to log to: File and Console
+            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = folder + filename };
+
+            // Rules for mapping loggers to targets            
+            if (debug)
+            {
+                config.AddRule(NLog.LogLevel.Debug, NLog.LogLevel.Fatal, logfile);
+            }
+            else config.AddRule(NLog.LogLevel.Info, NLog.LogLevel.Fatal, logfile);
+
+            //NLog.LogManager.Setup().SetupInternalLogger(s =>
+            //s.SetMinimumLogLevel(LogLevel.Trace).LogToFile(@"D:\Amir\Hello.txt"));
+            // Apply config           
+            NLog.LogManager.Configuration = config;
+            return NLog.LogManager.GetCurrentClassLogger();
+
+        }
+    }
+}
